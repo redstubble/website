7c69e7a122f2d7f80ee93e4e6cdba9016360e810
John <john@redstubble.com>
2018-06-01 16:44:50 +1200

    Site styling and custom domain name redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FaGithub, FaMedium, FaBeer, FaFileTextO } from 'react-icons/lib/fa';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,47 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <div class="clouds">
+          <div class="cloud-1" data-speed="35000" />
+          <div class="cloud-2" data-speed="45000" data-delay="15000" />{' '}
+          <h1 class="container logo">
+            <a class="link link--redstubble" href="#animation">
+              RE<span>D</span>STUBBLE
+            </a>
+          </h1>
+          <div class="cloud-3" data-speed="40000" />
+          <div class="cloud-4" data-speed="38000" data-delay="20000" />
+        </div>
+
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Sorry busy doing things, I will make this pretty soon I promise.<FaBeer />
         </p>
+        <p>
+          <a
+            href="https://github.com/redstubble"
+            target="_blank"
+            class="link-no-decoration"
+          >
+            <FaGithub size={50} style={{ margin: 30 }} />
+          </a>
+          <a
+            href="https://medium.com/@redstubble"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="link-no-decoration"
+          >
+            <FaMedium size={50} style={{ margin: 30 }} />
+          </a>
+          <a
+            href="https://github.com/redstubble/website/raw/gh-pages/resume.pdf"
+            rel="noopener noreferrer"
+            target="_blank"
+            class="link-no-decoration"
+          >
+            <FaFileTextO size={50} style={{ margin: 30 }} />
+          </a>
+        </p>
+        <p>john [at] redstubble [dot] com</p>
       </div>
     );
   }
